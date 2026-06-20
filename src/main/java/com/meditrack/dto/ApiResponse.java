f0e591ab891f54d8fd7f572720035b168fea33f3
Pratik Mulay <pratikmulay17@gmail.com>
2026-06-20 20:05:18 +0530

    Implement a generic API response class for consistent responses

---
+++ src/main/java/com/meditrack/dto/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.meditrack.dto;
+
+public class ApiResponse {
+    private boolean success;
+    private Object data;
+    private String message;
+
+    public ApiResponse() {}
+
+    public ApiResponse(boolean success, Object data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
+    public boolean isSuccess() { return success; }
+    public void setSuccess(boolean success) { this.success = success; }
+    
+    public Object getData() { return data; }
+    public void setData(Object data) { this.data = data; }
+    
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+}
