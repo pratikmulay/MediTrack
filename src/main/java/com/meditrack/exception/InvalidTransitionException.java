fc02493ac00217037a2b6cacd29893a3a1ed2c89
Pratik Mulay <pratikmulay17@gmail.com>
2026-06-20 20:05:18 +0530

    Create a custom exception for handling invalid state transitions

---
+++ src/main/java/com/meditrack/exception/InvalidTransitionException.java
@@ -0,0 +1,7 @@
+package com.meditrack.exception;
+
+public class InvalidTransitionException extends RuntimeException {
+    public InvalidTransitionException(String message) {
+        super(message);
+    }
+}
