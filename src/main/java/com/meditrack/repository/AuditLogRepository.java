1cb2547ab33f93cf6b18a70e22c003a2bb64f3e8
Pratik Mulay <pratikmulay17@gmail.com>
2026-06-27 13:36:17 +0530

    Add repository for saving audit log records

---
+++ src/main/java/com/meditrack/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.meditrack.repository;
+
+import com.meditrack.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByPatientId(Long patientId);
+}
